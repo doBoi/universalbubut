fdbd851b97a78774049be840ba64dbbfc7ebfa8d
doBoi <maulana4810@gmail.com>
2024-05-23 19:32:51 +0700

    fix contact

--- resources/views/components/pages/contact.blade.php
+++ resources/views/components/pages/contact.blade.php
@@ -2,7 +2,7 @@
     <h3 class="text-3xl text-center mt-24 -mb-24">Contact Us</h3>
     <div class="flex flex-col h-screen justify-center">
         <form action="https://api.whatsapp.com/send/" class="self-center" method="GET" target="_blank">
-            <input type="text" id="phone" name="phone" value="628979134310" required hidden><br>
+            <input type="text" id="phone" name="phone" value="6285810047212" required hidden><br>
             <button type="submit" class="w-full btn btn-success">WhatsApp</button>
         </form>
     </div>
