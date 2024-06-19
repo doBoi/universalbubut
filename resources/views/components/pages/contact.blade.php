3cff5967a6b7db2bd8cc51f9c2cd692adfde6886
doBoi <maulana4810@gmail.com>
2024-06-19 19:31:06 +0700

    improve contact section and data

--- resources/views/components/pages/contact.blade.php
+++ resources/views/components/pages/contact.blade.php
@@ -1,9 +1,12 @@
 <x-layouts.layout>
     <h3 class="text-3xl text-center mt-24 -mb-24">Contact Us</h3>
-    <div class="flex flex-col h-screen justify-center">
-        <form action="https://api.whatsapp.com/send/" class="self-center" method="GET" target="_blank">
+    <div class="flex md:flex-row flex-col-reverse h-screen justify-center ">
+        <form action="https://api.whatsapp.com/send/" class="self-center p-2 min-w-28" method="GET" target="_blank">
             <input type="text" id="phone" name="phone" value="6285810047212" required hidden><br>
-            <button type="submit" class="w-full btn btn-success">WhatsApp</button>
+            <button type="submit" class="w-full btn btn-success -mt-16">WhatsApp</button>
         </form>
+
+        <a href="https://maps.app.goo.gl/tptkFVC9jSexRjGh9" target="_blank"
+            class="self-center p-2 min-w-24 btn btn-primary mt-3">Maps</a>
     </div>
 </x-layouts.layout>
