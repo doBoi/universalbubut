fc64149c98b2df1dd15b03b864668a1767fed361
doBoi <maulana4810@gmail.com>
2025-04-02 12:57:09 +0700

    imporve card project

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,12 +1,8 @@
 {{-- Start Card --}}
-<figure class="px-10 pt-10">
+<figure class="px-2 pt-2">
     <img src="{{ $image }}" alt="{{ $title }}" class="rounded-xl" />
 </figure>
 <div class="card-body items-center text-center">
     <h2 class="card-title">{{ $title }}</h2>
-    <p>{{ $description }}</p>
-    <div class="card-actions">
-        <button class="btn btn-primary">Now</button>
-    </div>
 </div>
 {{-- End Card --}}
